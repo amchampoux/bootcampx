b04ccea5345d8e8ae417ac7acec8bd4e368b6e06
amchampoux <amchampoux@mirego.com>
2023-01-20 16:58:51 -0500

    connect bootcampx database

--- 4_queries/name_teachers_that_assisted.sql
+++ 4_queries/name_teachers_that_assisted.sql
@@ -1,6 +1,9 @@
-SELECT teachers.name as teacher
-FROM assistance_requests
-JOIN teachers ON teacher.id = teacher_id
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = student_id
+JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name = 'JUL02'
-GROUP BY teachers.name
-ORDER BY teachers.name
+ORDER BY teacher;
+
+
--- 4_queries/total_teacher_ar.sql
+++ 4_queries/total_teacher_ar.sql
@@ -1,5 +1,5 @@
-SELECT count(assistance_requests.*) as total_assistances, name
-FROM teachers
-JOIN assistance_requests on teachers.id = teacher_id
-WHERE name LIKE 'Waylon Boehm'
-GROUP BY name;
+SELECT count(assistance_requests.*) as total_assistances, teachers.name
+FROM assistance_requests
+JOIN teachers ON teachers.id = teacher_id
+WHERE name = 'Waylon Boehm'
+GROUP BY teachers.name;
